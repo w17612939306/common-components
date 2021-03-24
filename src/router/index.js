368a6f46db911813b2fb7c2e2c2c94ec234e3559
Chinasoft_Broker/zhudongsheng <zhudongsheng@chinasoftinc.com>
2021-03-24 17:22:43 +0800

    修改入口路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Bread from '@views/table/Index'
+import Bread from '@views/home/Index'
 
 Vue.use(Router)
 
